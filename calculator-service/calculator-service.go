20aef0fd33a1ad67e79c00bee83fa57fdc98ea1c
shyampundkar <shyam.pundkar@gmail.com>
2021-11-29 12:00:40 +1100

    added a default handler

--- calculator-service/calculator-service.go
+++ calculator-service/calculator-service.go
@@ -3,12 +3,17 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func main() {
 
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 	http.HandleFunc("/calculate/", func(rw http.ResponseWriter, r *http.Request) {
 		operation := r.URL.Query().Get("operation")
 		operand1, _ := strconv.Atoi(r.URL.Query().Get("operand1"))
@@ -20,7 +25,7 @@ func main() {
 			fmt.Fprintf(rw, "<h1> The result is:%d</h1> ", result)
 		}
 	})
-	http.ListenAndServe("localhost:8010", nil)
+	log.Fatal(http.ListenAndServe("localhost:8010", nil))
 }
 
 func Calculate(operation string, arg1, arg2 int) (int, error) {
