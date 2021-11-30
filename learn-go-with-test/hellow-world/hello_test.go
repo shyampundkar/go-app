ee9d968f8cce799e00b898870940215427b1b529
shyampundkar <shyam.pundkar@gmail.com>
2021-11-30 14:23:04 +1100

    Update hello_test.go
    sub test with inner function asserting the validation

--- learn-go-with-test/hellow-world/hello_test.go
+++ learn-go-with-test/hellow-world/hello_test.go
@@ -13,23 +13,25 @@ import "testing"
 
 func TestHellow(t *testing.T) {
 
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
 	t.Run("saying hellow to people", func(t *testing.T) {
 		got := Hello("Shyam")
 		want := "Hello, Shyam!"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 
 		got := Hello("")
-		want := "Hello, World!"
+		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
-
+		assertCorrectMessage(t, got, want)
 	})
 }
