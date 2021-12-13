bb5210ce75c919d225efc32019b256b524c204d6
shyampundkar <shyam.pundkar@gmail.com>
2021-12-13 12:59:25 +1100

    Update InterestCalculator_test.go
    reverting the change to make build pass

--- LoanCalculator/go/InterestCalculator_test.go
+++ LoanCalculator/go/InterestCalculator_test.go
@@ -12,7 +12,7 @@ func TestCalculateRepayment(t *testing.T) {
 		var want float64 = 2109
 		got := math.Round(InterestCalculator.CalculateRepayment(7.23, 1, 350000, 12))
 
-		if got == want {
+		if got != want {
 			t.Errorf("got: %f want: %f", got, want)
 		}
 	})
